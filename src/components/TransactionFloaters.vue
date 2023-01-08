fd2e07c9d9abbe637ed684dd9d4563e6ee7169cf
luiztsmelo <luiztsmelo@gmail.com>
2023-01-08 20:26:20 -0300

    Fixes

--- src/components/TransactionFloaters.vue
+++ src/components/TransactionFloaters.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-import { PlusIcon, MinusIcon } from '@heroicons/vue/24/solid'
 </script>
 
 <template>
   <div class="fixed left-6 right-6 bottom-6 flex items-center justify-center gap-12">
     <button
-      class="flex items-center justify-center bg-black p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
+      class="flex items-center justify-center bg-white p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
     >
-      <PlusIcon class="text-white"></PlusIcon>
+      <span class="material-icons-round !text-4xl text-blue-700">add</span>
     </button>
     <button
-      class="flex items-center justify-center bg-black p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
+      class="flex items-center justify-center bg-white p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
     >
-      <MinusIcon class="text-white"></MinusIcon>
+      <span class="material-icons-round !text-4xl text-red-700">remove</span>
     </button>
   </div>
 </template>
