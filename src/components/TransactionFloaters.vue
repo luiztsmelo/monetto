8293ed683ff07e6fbb9f22c1662fff672145b51d
luiztsmelo <luiztsmelo@gmail.com>
2023-01-08 20:32:58 -0300

    Fix floaters

--- src/components/TransactionFloaters.vue
+++ src/components/TransactionFloaters.vue
@@ -3,14 +3,14 @@
 <template>
   <div class="fixed left-6 right-6 bottom-6 flex items-center justify-center gap-12">
     <button
-      class="flex items-center justify-center bg-white p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
+      class="flex items-center justify-center bg-black p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
     >
-      <span class="material-icons-round !text-4xl text-blue-700">add</span>
+      <span class="material-icons-round !text-4xl text-white">add</span>
     </button>
     <button
-      class="flex items-center justify-center bg-white p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
+      class="flex items-center justify-center bg-black p-4 h-16 w-16 rounded-full active:scale-[0.97] transition duration-[200] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
     >
-      <span class="material-icons-round !text-4xl text-red-700">remove</span>
+      <span class="material-icons-round !text-4xl text-white">remove</span>
     </button>
   </div>
 </template>
