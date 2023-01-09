481ee922116fa1d19f284bc21a62f7edff7a3f59
luiztsmelo <luiztsmelo@gmail.com>
2023-01-08 23:40:59 -0300

    Modal WIP

--- src/components/TransactionFloaters.vue
+++ src/components/TransactionFloaters.vue
@@ -1,4 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Money3Component as InputMoney } from 'v-money3'
+
+const modal = ref(false)
+const amount = ref(0)
+const config = {
+  masked: false,
+  prefix: 'R$',
+  suffix: '',
+  thousands: '.',
+  decimal: ',',
+  precision: 2,
+  disableNegative: false,
+  disabled: false,
+  min: null,
+  max: null,
+  allowBlank: false,
+  minimumNumberOfCharacters: 0
+}
+</script>
 
 <template>
   <div class="fixed left-6 right-6 bottom-6 flex items-center justify-center gap-12">
@@ -8,9 +28,44 @@
       <span class="material-icons-round !text-4xl text-white">add</span>
     </button>
     <button
-      class="flex items-center justify-center bg-black p-4 h-16 w-16 rounded-full active:scale-[0.93] transition duration-[1000] ease drop-shadow-xl shadow-5xl"
+      class="flex items-center justify-center bg-black p-4 h-16 w-16 rounded-full active:scale-[0.93] transition duration-[250] ease-[cubic-bezier(.4,0,.2,1)] drop-shadow-md shadow-xl"
+      @click="modal = true"
     >
       <span class="material-icons-round !text-4xl text-white">remove</span>
     </button>
   </div>
+
+  <Transition name="fade">
+    <div class="fixed left-0 right-0 bottom-0 w-full h-full bg-[rgba(0,0,0,0.3)] z-40" v-if="modal"></div>
+  </Transition>
+
+  <Transition name="slide-y">
+    <div class="fixed left-0 right-0 bottom-0 w-full min-h-[50%] rounded-4xl bg-white p-6 z-50" v-if="modal">
+      <InputMoney v-model="amount" v-bind="config" class="w-full rounded-lg p-3 bg-gray-100 text-2xl">{{
+        amount
+      }}</InputMoney>
+    </div>
+  </Transition>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s cubic-bezier(0.33, 1, 0.68, 1);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-y-enter-active,
+.slide-y-leave-active {
+  transition: all 0.4s cubic-bezier(0.33, 1, 0.68, 1);
+}
+
+.slide-y-enter-from,
+.slide-y-leave-to {
+  transform: translateY(100%);
+}
+</style>
